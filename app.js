bd22c330f03554a91ccc0dcb51fa7936867ccf64
Christian Farmer <christian.farmer@paymentspring.com>
2023-03-26 17:20:03 -0500

    Improve endpoint comments

--- app.js
+++ app.js
@@ -4,20 +4,52 @@ var app = express()
 app.use(express.json())
 
 app.get("/", async function(request, response) {
+  /**
+   * Route: / ("Root Route")
+   *
+   * Response: Text, Static (will be the same every time)
+   *
+   * URL Example: http://localhost:3000
+   */
+
   response.end("Welcome to my API")
 })
 
 app.get("/hello", async function(request, response) {
+  /**
+   * Route: /hello
+   *
+   * Response: Text, Static (will be the same every time)
+   *
+   * URL Example: http://localhost:3000/hello
+   */
+
   response.end("Hello. How are you?")
 })
 
 app.get("/john-greeting", async function(request, response) {
+  /**
+   * Route: /john-greeting
+   *
+   * Response: Text, Static (will be the same every time)
+   *
+   * URL Example: http://localhost:3000/john-greeting
+   */
+
   var name = "John"
   var greeting = "Hello, " + name + ", how are you?"
   response.end(greeting)
 })
 
 app.get("/random-greeting", async function(request, response) {
+  /**
+   * Route: /random-greeting
+   *
+   * Response: Text, Dynamic
+   *
+   * URL Example: http://localhost:3000/random-greeting
+   */
+
   var names = ["John", "Jane", "Chad"]
   var randomIndex = Math.floor(Math.random() * 3)
   var name = names[randomIndex]
@@ -26,10 +58,40 @@ app.get("/random-greeting", async function(request, response) {
   response.end(greeting)
 })
 
+app.get("/users/:userId", async function(request, response) {
+  /**
+   * This endpoint shows how to access URL Path Parameters
+   *
+   * Route: /users/:userId
+   *
+   * Response: Text, Dynamic
+   *
+   * URL Example: http://localhost:3000/users/1
+   *
+   * # GET Request Object Structure w/ Path Params:
+   * var request = {
+   *   query: {
+   *     userId: "1",
+   *   }
+   * }
+   */
+  var id = request.params.userId
+
+  var greeting = "Hello, your user ID is: " + id
+  response.end(greeting)
+})
+
 app.get("/get-req-greeting", async function(request, response) {
   /**
-   * # GET Request Structure:
-   * # Assuming URL query: http://localhost:3000/get-req-greeting?name=John&age=25&extra-stuff=Meow
+   * This endpoint shows how to access URL Query Parameters
+   *
+   * Route: /get-req-greeting
+   *
+   * Response: Text, Dynamic
+   *
+   * URL Example: http://localhost:3000/get-req-greeting?name=John&age=25&extra-stuff=Meow
+   *
+   * # GET Request Object Structure:
    * var request = {
    *   query: {
    *     name: "John",
@@ -49,17 +111,25 @@ app.get("/get-req-greeting", async function(request, response) {
 
 app.post("/users", async function(request, response) {
   /**
+   * This endpoint shows how to access JSON data in the POST Request body
+   *
    * NOTE: This will not work unless requests contain the
    * Content-Type header configured as follows:
+   *   "Content-Type: application/json"
    *
-   * Content-Type: application/json
+   * Route: /users
    *
-   * # POST Request Structure:
-   * # Assuming JSON body: { name: "John", age: 25 }
+   * Response: Text, Dynamic
+   *
+   * URL Example: http://localhost:3000/users
+   *
+   * # POST Request Object Structure:
+   * # Assuming JSON body: { name: "John", age: 25, extraStuff: "Meow" }
    * var request = {
    *   body: {
    *     name: "John",
-   *     age: 25
+   *     age: 25,
+   *     extraStuff: "Meow",
    *   }
    * }
    */
